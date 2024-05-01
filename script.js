5c5e873e8252fae9245350b9a38bfce9852ae8b2
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 15:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let divs = document.querySelectorAll('.image');
+
+divs.forEach((div) => {
+  div.addEventListener('dragstart', function (e) {
+    e.dataTransfer.setData('text/plain', this.id);
+  });
+
+  div.addEventListener('dragover', function (e) {
+    e.preventDefault(); // This is necessary to allow dropping
+  });
+
+  div.addEventListener('drop', function (e) {
+    e.preventDefault(); // This is necessary to allow dropping
+
+    const draggedId = e.dataTransfer.getData('text/plain');
+    const draggedElement = document.getElementById(draggedId);
+
+    // Get the background images
+    const draggedImage = window.getComputedStyle(draggedElement).backgroundImage;
+    const targetImage = window.getComputedStyle(this).backgroundImage;
+
+    // Swap the images
+    draggedElement.style.backgroundImage = targetImage;
+    this.style.backgroundImage = draggedImage;
+  });
+});
